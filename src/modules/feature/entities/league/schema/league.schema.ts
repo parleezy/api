d7475a4f5b053723a9a855e71fde2acf5bc5cdb1
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-03-13 17:16:41 +0400

    feat: cleanup

--- src/modules/feature/entities/league/schema/league.schema.ts
+++ src/modules/feature/entities/league/schema/league.schema.ts
@@ -1,14 +1,14 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 
 // Schema
+import { LeagueExternal, LeagueExternalSchema } from './league-external.schema'
 import { LeagueIdentity, LeagueIdentitySchema } from './league-identity.schema'
+import { LeagueLocation, LeagueLocationSchema } from './location/league-location.schema'
 import { LeagueSettings, LeagueSettingsSchema } from './league-settings.schema'
 import { LeagueSocials, LeagueSocialsSchema } from './league-socials.schema'
 
 // Types
 import { Api } from '@/data/types/api'
-import { LeagueExternal, LeagueExternalSchema } from './league-external.schema'
-import { LeagueLocation, LeagueLocationSchema } from './location/league-location.schema'
 
 export type LeagueDocument = League & Document
 
