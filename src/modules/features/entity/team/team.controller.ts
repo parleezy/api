8d44fd2006c57943a3f6f181bb163d96b6563084
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-03-20 00:57:38 +0400

    feat: list teams

--- src/modules/features/entity/team/team.controller.ts
+++ src/modules/features/entity/team/team.controller.ts
@@ -1,9 +1,25 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 
 // Service
 import { TeamService } from './team.service'
 
+// Api
+import { Api } from '@/data/types/api'
+
+// Team
+import { Team } from './schema/team.schema'
+
 @Controller('teams')
 export class TeamController {
     constructor(private _teamService: TeamService) {}
+
+    @Get()
+    async list(): Promise<Api.Response<Team[]>> {
+        const data = await this._teamService.list()
+
+        return {
+            data,
+            success: true,
+        }
+    }
 }
--- src/modules/features/entity/team/team.service.ts
+++ src/modules/features/entity/team/team.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common'
 
 // Team
+import { Team } from './schema/team.schema'
 import { TeamRepository } from './team.repository'
 
 @Injectable()
 export class TeamService {
     constructor(private _teamRepository: TeamRepository) {}
+
+    async list(): Promise<Team[]> {
+        return await this._teamRepository.list()
+    }
 }
