b9a3b548fa71d4cdcb9ec515e525044751c01aa6
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-03-25 14:50:48 +0400

    feat: init controller and repository

--- src/modules/league/league.module.ts
+++ src/modules/league/league.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
 
-// Schema
+// League
 import { League, LeagueSchema } from './league.schema'
+import { LeagueController } from './league.controller'
+import { LeagueRepository } from './league.repository'
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: League.name, schema: LeagueSchema }])],
-    controllers: [],
-    providers: [],
+    controllers: [LeagueController],
+    providers: [LeagueRepository],
     exports: [],
 })
 export class LeagueModule {}
