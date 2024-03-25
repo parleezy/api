d53a799b0e3107e1ad182ecc81c017621e3667da
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-03-25 16:12:40 +0400

    feat: competition init crud

--- src/modules/competition/competition.controller.ts
+++ src/modules/competition/competition.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, Post } from '@nestjs/common'
+
+// Api
+import { Api } from '@/models/api'
+
+// Competition
+import { Competition } from './competition.schema'
+import { CompetitionService } from './competition.service'
 
 @Controller('competitions')
-export class CompetitionController {}
+export class CompetitionController {
+    constructor(private _competitionService: CompetitionService) {}
+
+    @Get()
+    async list(): Promise<Api.Response<Competition[]>> {
+        const data = await this._competitionService.list()
+
+        return {
+            data,
+            success: true,
+        }
+    }
+
+    @Post()
+    async create(): Promise<Api.Response<Competition>> {
+        const data = await this._competitionService.create()
+
+        return {
+            data,
+            success: true,
+        }
+    }
+
+    @Get(`:id`)
+    async getById(@Param('id') id: string): Promise<Api.Response<Competition>> {
+        const data = await this._competitionService.retrieve(id)
+
+        return {
+            data,
+            success: true,
+        }
+    }
+}
