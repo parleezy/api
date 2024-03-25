2726af98604eef0440ae0612cd72de560c691885
Jason McFarlane <jmcfarlane.developer@gmail.com>
2024-03-25 15:29:37 +0400

    feat: create, list retrieve init

--- src/modules/league/league.controller.ts
+++ src/modules/league/league.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, Post } from '@nestjs/common'
+
+// Api
+import { Api } from '@/models/api'
+
+// League
+import { League } from './league.schema'
+import { LeagueService } from './league.service'
 
 @Controller('leagues')
-export class LeagueController {}
+export class LeagueController {
+    constructor(private _leagueService: LeagueService) {}
+
+    @Get()
+    async list(): Promise<Api.Response<League[]>> {
+        const data = await this._leagueService.list()
+
+        return {
+            data,
+            success: true,
+        }
+    }
+
+    @Post()
+    async create(): Promise<Api.Response<League>> {
+        const data = await this._leagueService.create()
+
+        return {
+            data,
+            success: true,
+        }
+    }
+
+    @Get(`:id`)
+    async getById(@Param('id') id: string): Promise<Api.Response<League>> {
+        const data = await this._leagueService.retrieve(id)
+
+        return {
+            data,
+            success: true,
+        }
+    }
+}
--- src/modules/league/league.service.ts
+++ src/modules/league/league.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common'
+import { LeagueRepository } from './league.repository'
+import { League } from './league.schema'
 
 @Injectable()
-export class LeagueService {}
+export class LeagueService {
+    constructor(private _leagueRepository: LeagueRepository) {}
+
+    create(): Promise<League> {
+        return this._leagueRepository.create({})
+    }
+
+    list(): Promise<League[]> {
+        return this._leagueRepository.list()
+    }
+
+    retrieve(id: string): Promise<League> {
+        return this._leagueRepository.retrieve(id)
+    }
+}
